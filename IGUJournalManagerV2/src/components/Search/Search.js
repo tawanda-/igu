13c814e5be2373d9fa4a5d24bf728d7e35281fb8
Ngoni Munyaradzi <ngoni.munyaradzi@gmail.com>
2018-10-28 16:50:36 +0200

    testing modifications

--- IGUJournalManagerV2/src/components/Search/Search.js
+++ IGUJournalManagerV2/src/components/Search/Search.js
@@ -28,7 +28,7 @@ class Search extends Component {
                 <p>Search Component</p>
                 <input className="Searchbox"
                     type="text"
-                    placeholder='Enter Search Term'
+                    placeholder='Search By Country'
                     value={this.state.searchTerm}
                     onChange={this.handleChange} />
 

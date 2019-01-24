92de2c8cad65c598b64de159c30426828f005f72
Tawanda <tmuhwati@gmail.com>
2019-01-24 17:28:38 +0200

    dev changes

---
+++ journals-manager/frontend/journalsmanagerfrontend/build/precache-manifest.2f863a92685b8b2b6738af6d4abb6ec4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c29d60e73c55d8f848c3",
+    "url": "/static/js/main.c29d60e7.chunk.js"
+  },
+  {
+    "revision": "09a4b660f6ff500d70b9",
+    "url": "/static/js/1.09a4b660.chunk.js"
+  },
+  {
+    "revision": "c29d60e73c55d8f848c3",
+    "url": "/static/css/main.ccae705a.chunk.css"
+  },
+  {
+    "revision": "94a195d0ffa5d2ccfbf801a27a5b75cf",
+    "url": "/index.html"
+  }
+];

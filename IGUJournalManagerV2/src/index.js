5979f83b415914297898455ef220f2760dedd8cd
Tawanda <tmuhwati@gmail.com>
2019-01-28 22:31:18 +0200

    Changed imports

--- IGUJournalManagerV2/src/index.js
+++ IGUJournalManagerV2/src/index.js
@@ -1,6 +1,6 @@
-import React from 'react';
 import 'babel-polyfill'; 
 import 'react-app-polyfill/ie11';
+import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';

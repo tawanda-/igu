b5fc9c0caa8db770f0cc3442defb83a05c8d6632
Tawanda <tmuhwati@gmail.com>
2019-02-17 17:02:47 +0200

    Changes

--- IGUJournalsAdmin/src/components/viewJournals/index.js
+++ IGUJournalsAdmin/src/components/viewJournals/index.js
@@ -31,9 +31,32 @@ import DeleteIcon from '@material-ui/icons/Delete';
 import EditIcon from '@material-ui/icons/Edit';
 import SaveIcon from '@material-ui/icons/Save';
 import CancelIcon from '@material-ui/icons/Cancel';
+import UpdateIcon from '@material-ui/icons/Update';
 import { withStyles } from '@material-ui/core/styles';
 
 import { Loading } from '../../theme-sources/material-ui/components/loading';
+
+import { Modal, Upload, Icon } from 'antd';
+
+const Dragger = Upload.Dragger;
+
+const bulkUploadProps = {
+  name: 'file',
+  multiple: false,
+    //action: 'https://igu-online.org/wp-admin/admin-ajax.php',
+    action: 'http://www.esikolweni.co.za/wp-admin/admin-ajax.php',
+  onChange(info) {
+    const status = info.file.status;
+    if (status !== 'uploading') {
+      console.log(info.file, info.fileList);
+    }
+    /*if (status === 'done') {
+      message.success(`${info.file.name} file uploaded successfully.`);
+    } else if (status === 'error') {
+      message.error(`${info.file.name} file upload failed.`);
+    }*/
+  },
+};
 
 const styles = theme => ({
     lookupEditCell: {
@@ -156,22 +179,22 @@ class viewJournals extends Component{
 
     this.state = {
 
-    columns : [
-        { name: 'id' , title: 'id'},
-        { name: 'country' , title: 'Country'},
-        { name: 'name_of_journal' , title: 'Journal Name'},
-        { name: 'print_issn' , title: 'Print ISSN'},
-        { name: 'e_issn' , title: 'eISSN'},
-        { name: 'city_of_publication' , title: 'City of Publication'},
-        { name: 'name_of_publishing_company' , title: 'Publishing Company'},
-        { name: 'editor' , title: 'Editor'},
-        { name: 'editor_email_address' , title: 'Editor email Addres'},
-        { name: 'language' , title: 'Publication language'},
-        { name: 'since' , title: 'Since'},
-        { name: 'isi' , title: 'ISI'},
-        { name: 'isi_category' , title: 'ISI Category'},
-        { name: '5_year_impact_factor' , title: '5 Year Impact Factor'}
-    ],
+      columns : [
+          { name: 'id' , title: 'id'},
+          { name: 'country' , title: 'Country'},
+          { name: 'name_of_journal' , title: 'Journal Name'},
+          { name: 'print_issn' , title: 'Print ISSN'},
+          { name: 'e_issn' , title: 'eISSN'},
+          { name: 'city_of_publication' , title: 'City of Publication'},
+          { name: 'name_of_publishing_company' , title: 'Publishing Company'},
+          { name: 'editor' , title: 'Editor'},
+          { name: 'editor_email_address' , title: 'Editor email Addres'},
+          { name: 'language' , title: 'Publication language'},
+          { name: 'since' , title: 'Since'},
+          { name: 'isi' , title: 'ISI'},
+          { name: 'isi_category' , title: 'ISI Category'},
+          { name: '5_year_impact_factor' , title: '5 Year Impact Factor'}
+      ],
       tableColumnExtensions: [
         /*{ columnName: 'country', width: 100 },
         { columnName: 'name_of_journal', width: 100 },
@@ -190,6 +213,8 @@ class viewJournals extends Component{
       rowChanges: {},
       deletingRows: [],
       selection: [],
+
+      showUploadModal: false
       
     };
 
@@ -198,150 +223,186 @@ class viewJournals extends Component{
     this.changeSorting = this.changeSorting.bind(this);
 
     const getStateDeletingRows = () => {
-        const { deletingRows } = this.state;
-        return deletingRows;
-      };
-      const getStateRows = () => {
-        const { rows } = this.state;
-        return rows;
+      const { deletingRows } = this.state;
+      return deletingRows;
+    };
+    
+    const getStateRows = () => {
+      const { rows } = this.state;
+      return rows;
+    };
+  
+  this.changeSorting = sorting => this.setState({ sorting });
+
+  this.changeEditingRowIds = editingRowIds => this.setState({ editingRowIds });
+
+  this.changeAddedRows = addedRows => this.setState({
+    addedRows: addedRows.map(row => (Object.keys(row).length ? row : {
+        country:'',
+        name_of_journal:'',
+        print_issn:'',
+        e_issn:'',
+        city_of_publication:'',
+        name_of_publishing_company:'',
+        editor:'',
+        editor_email_address:'',
+        language:'',
+        since:0,
+        isi:0,
+        isi_category:'',
+    })),
+  });
+
+  this.changeRowChanges = rowChanges => this.setState({ rowChanges });
+
+  this.commitChanges = ({ added, changed, deleted }) => {
+
+    let { rows } = this.state;
+
+    if (added) {
+      const startingAddedId = rows.length > 0 ? rows[rows.length - 1].id + 1 : 0;
+      rows = [
+        ...rows,
+        ...added.map((row, index) => ({
+          id: startingAddedId + index,
+          ...row,
+        })),
+      ];
+
+      const querystring = require('querystring');
+      
+      const searchParams = {
+        action: 'the_ajax_hook',
+        filter: 'insert',
+        payload: JSON.stringify(added)
       };
   
-      this.changeSorting = sorting => this.setState({ sorting });
-      this.changeEditingRowIds = editingRowIds => this.setState({ editingRowIds });
-      this.changeAddedRows = addedRows => this.setState({
-        addedRows: addedRows.map(row => (Object.keys(row).length ? row : {
-            country:'',
-            name_of_journal:'',
-            print_issn:'',
-            e_issn:'',
-            city_of_publication:'',
-            name_of_publishing_company:'',
-            editor:'',
-            editor_email_address:'',
-            language:'',
-            since:0,
-            isi:0,
-            isi_category:'',
-        })),
+      const request = new Request(
+        'http://localhost/igu/wp-admin/admin-ajax.php',{
+          method: 'POST',
+          headers: {'Accept':'*/*', 'Content-Type': 'application/x-www-form-urlencoded'},
+          //headers: {'Accept':'*/*', "Access-Control-Allow-Origin": "*", 'Content-Type': 'application/json;charset=UTF-8'},
+          body: querystring.stringify(searchParams),
+          //body: JSON.stringify(searchParams)
       });
-      this.changeRowChanges = rowChanges => this.setState({ rowChanges });
-      this.commitChanges = ({ added, changed, deleted }) => {
-
-        let { rows } = this.state;
-
-        if (added) {
-          const startingAddedId = rows.length > 0 ? rows[rows.length - 1].id + 1 : 0;
-          rows = [
-            ...rows,
-            ...added.map((row, index) => ({
-              id: startingAddedId + index,
-              ...row,
-            })),
-          ];
-
-          const querystring = require('querystring');
-          
-          const searchParams = {
-            action: 'the_ajax_hook',
-            filter: 'insert',
-            payload: JSON.stringify(added)
-          };
-      
-          const request = new Request(
-            'http://localhost/igu/wp-admin/admin-ajax.php',{
-              method: 'POST',
-              headers: {'Accept':'*/*', 'Content-Type': 'application/x-www-form-urlencoded'},
-              //headers: {'Accept':'*/*', "Access-Control-Allow-Origin": "*", 'Content-Type': 'application/json;charset=UTF-8'},
-              body: querystring.stringify(searchParams),
-              //body: JSON.stringify(searchParams)
-          });
-      
-          return fetch(request)
-            .then(response => response.json())
-            .then(response => {
-              console.log(response);
-              //this.setState({rows:response, loading: false});
-            }
-            ).catch(error => {
-              return error;
-            });
+  
+      return fetch(request)
+        .then(response => response.json())
+        .then(response => {
+          console.log(response);
+          //this.setState({rows:response, loading: false});
         }
-
-        if (changed) {
-
-          rows = rows.map(row => (changed[row.id] ? { ...row, ...changed[row.id] } : row));
-
-          const querystring = require('querystring');
-
-          const searchParams = {
-            action: 'the_ajax_hook',
-            filter: 'update',
-            payload: JSON.stringify(changed)
-          };
-      
-          const request = new Request(
-            'http://localhost/igu/wp-admin/admin-ajax.php',{
-              method: 'POST',
-              headers: {'Accept':'*/*', 'Content-Type': 'application/x-www-form-urlencoded'},
-              body: querystring.stringify(searchParams),
-          });
-      
-          return fetch(request)
-            .then(response => response.json())
-            .then(response => {
-              this.setState({rows:response, loading: false});
-            }
-            ).catch(error => {
-              return error;
-            });
-        }
-
-        if(deleted){
-
-          this.setState({ rows, deletingRows: deleted || getStateDeletingRows() });
-
-          const querystring = require('querystring');
-
-          const searchParams = {
-            action: 'the_ajax_hook',
-            filter: 'delete',
-            payload: JSON.stringify(deleted)
-          };
-      
-          const request = new Request(
-            'http://localhost/igu/wp-admin/admin-ajax.php',{
-              method: 'POST',
-              headers: {'Accept':'*/*', 'Content-Type': 'application/x-www-form-urlencoded'},
-              body: querystring.stringify(searchParams),
-          });
-      
-          return fetch(request)
-            .then(response => response.json())
-            .then(response => {
-              this.setState({rows:response, loading: false});
-            }
-            ).catch(error => {
-              return error;
-            });
-        }
-      };
-
-      this.cancelDelete = () => this.setState({ deletingRows: [] });
-      this.deleteRows = () => {
-        const rows = getStateRows().slice();
-        getStateDeletingRows().forEach((rowId) => {
-          const index = rows.findIndex(row => row.id === rowId);
-          if (index > -1) {
-            rows.splice(index, 1);
-          }
+        ).catch(error => {
+          return error;
         });
-        this.setState({ rows, deletingRows: [] });
-      };
+    }
 
-      this.changeColumnOrder = (order) => {
-        this.setState({ columnOrder: order });
+    if (changed) {
+
+      rows = rows.map(row => (changed[row.id] ? { ...row, ...changed[row.id] } : row));
+
+      const querystring = require('querystring');
+
+      console.log(changed);
+
+      const searchParams = {
+        action: 'the_ajax_hook',
+        filter: 'update',
+        payload: JSON.stringify(changed)
+        //payload: changed
       };
+  
+      const request = new Request(
+        'http://localhost/igu/wp-admin/admin-ajax.php',{
+          method: 'POST',
+          headers: {'Accept':'*/*', 'Content-Type': 'application/x-www-form-urlencoded'},
+          //headers: {'Accept':'*/*','Content-Type': 'application/json'},
+          body: querystring.stringify(searchParams),
+          //body: JSON.stringify(searchParams),
+      });
+
+      console.log(searchParams);
+      console.log(querystring.stringify(searchParams));
+  
+      return fetch(request)
+        .then(response => response.json())
+        .then(response => {
+          this.setState({rows:response, loading: false});
+        }
+        ).catch(error => {
+          return error;
+        });
+    }
+
+    if(deleted){
+
+      this.setState({ rows, deletingRows: deleted || getStateDeletingRows() });
+
+      const querystring = require('querystring');
+
+      const searchParams = {
+        action: 'the_ajax_hook',
+        filter: 'delete',
+        payload: JSON.stringify(deleted)
+      };
+  
+      const request = new Request(
+        'http://localhost/igu/wp-admin/admin-ajax.php',{
+          method: 'POST',
+          headers: {'Accept':'*/*', 'Content-Type': 'application/x-www-form-urlencoded'},
+          body: querystring.stringify(searchParams),
+      });
+  
+      return fetch(request)
+        .then(response => response.json())
+        .then(response => {
+          this.setState({rows:response, loading: false});
+        }
+        ).catch(error => {
+          return error;
+        });
+    }
+  };
+
+  this.cancelDelete = () => this.setState({ deletingRows: [] });
+
+  this.deleteRows = () => {
+    const rows = getStateRows().slice();
+    getStateDeletingRows().forEach((rowId) => {
+      const index = rows.findIndex(row => row.id === rowId);
+      if (index > -1) {
+        rows.splice(index, 1);
+      }
+    });
+    this.setState({ rows, deletingRows: [] });
+  };
+
+  this.changeColumnOrder = (order) => {
+    this.setState({ columnOrder: order });
+  };
+
+  this.onClickUploadButton = () => {
+    console.log("on click");
+    this.setState({
+      showUploadModal: true,
+    });
   }
+
+  this.uploadModalHandleOk = (e) => {
+    console.log(e);
+    this.setState({
+      visible: false,
+    });
+  }
+
+  this.uploadModalHandleCancel = (e) => {
+    console.log(e);
+    this.setState({
+      showUploadModal: false,
+    });
+  }
+
+}
 
   componentDidMount() {
     this.loadData();
@@ -392,12 +453,11 @@ class viewJournals extends Component{
     return fetch(request)
       .then(response => response.json())
       .then(response => {
-        console.log(response);
         this.setState({rows:response, loading: false});
-        })
+      })
       .catch(() => this.setState({ loading: false }));
   }
-
+  
   render() {
     const {
         classes,
@@ -415,6 +475,8 @@ class viewJournals extends Component{
 
     return (
       <Paper style={{ position: 'relative' }}>
+
+        <Button onClick={this.onClickUploadButton} color="primary">Bulk Upload</Button>
 
         <Grid
           rows={rows}
@@ -498,6 +560,32 @@ class viewJournals extends Component{
             </Button>
             <Button onClick={this.deleteRows} color="secondary">
               Delete
+            </Button>
+          </DialogActions>
+        </Dialog>
+
+        <Dialog
+          open={this.state.showUploadModal}
+          onClose={this.uploadModalHandleCancel}
+          classes={{ paper: classes.dialog }}
+        >
+          <DialogTitle>
+            Bulk Upload
+          </DialogTitle>
+          <DialogContent>
+            <Dragger {...bulkUploadProps}>
+              <p className="ant-upload-drag-icon">
+                <Icon type="inbox" />
+              </p>
+              <p className="ant-upload-text">Click or drag file to this area to upload</p>
+            </Dragger>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={this.uploadModalHandleCancel} color="primary">
+              Cancel
+            </Button>
+            <Button onClick={this.uploadModalHandleOk} color="secondary">
+              Upload
             </Button>
           </DialogActions>
         </Dialog>

b307aeb10b8b9404669473d26e9b042210a9846d
Tawanda <tmuhwati@gmail.com>
2019-02-25 08:22:28 +0200

    Working version

---
+++ IGUJournalsAdmin/src/components/viewJournals/settings.js
@@ -0,0 +1,4 @@
+export const LOCAL_BASE_URL = 'http://localhost/igu/wp-admin/admin-ajax.php';
+export const DEV_BASE_URL = 'https://igu.esikolweni.co.za/wp-admin/admin-ajax.php';
+export const PROD_BASE_URL = 'https://igu-online.org/wp-admin/admin-ajax.php';
+export const WP_ACTION = "the_ajax_hook";
